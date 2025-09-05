a29d5c202bfba581b5a2b34822e3487eceafcc77
mutms7 <welcomehomemutms7@gmail.com>
2025-09-05 17:39:58 -0400

    m

---
+++ A2a.rkt
@@ -0,0 +1,4 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname A2a) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define (collinear a x b y c z) (or (and (= (- x y) 0) (= (- y z) 0))(= (/ (- a b) (- x y)) (/ (- b c) (- y z)))))
