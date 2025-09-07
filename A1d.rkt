78865d9cf5adc17cae9994deef9f9d8c224f33bb
mutms7 <welcomehomemutms7@gmail.com>
2025-09-07 15:57:19 -0400

    m

---
+++ A1d.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname A1d) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #t () #f)))
+(define step-count-f 4)
+(define step-count-g 5)
+(define step-count-h 5)
+(define step-count-i 5)
+(define step-count-j 5)
