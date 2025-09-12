5ee305c27fe8c0bd5f0b92fbf8acdad2e928a195
mutms7 <welcomehomemutms7@gmail.com>
2025-09-12 16:23:43 -0400

    w

--- A2a.rkt
+++ A2a.rkt
@@ -3,8 +3,8 @@
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname A2a) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 (define (step-count-k n) (+ 3 (* n 5)))
 (define (step-count-l n) (if (= n 0) 3 (+ 6 (* 2 (step-count-l (sub1 n))))))
-(define (step-count-m n) (if (= n 0) 3 (+ 3 (* (+ 1 (inexact->exact(log n 2))) 5))))
-(define (step-count-n n) (if (< n 1) 3 (+ 3 (* (+ 1 (floor (inexact->exact(log n 2)))) 5))))
+(define (step-count-m n) (if (= n 0) 3 (+ 3 (* (nhelper n) 5))))
+(define (step-count-n n) (if (< n 1) 3 (+ 3 (* (nhelper n) 5))))
+(define (nhelper n) (if (= n 0) 0 (+ 1 (nhelper (quotient n 2)))))
 (define (step-count-o n) (if (< n 1) 3 (+ 6 (* 2 (step-count-o (quotient n 2))))))
 
-(check-expect (step-count-n 100) 38)
--- A2b.rkt
+++ A2b.rkt
@@ -1,8 +1,29 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname A2b) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-(define (step-count-k n) (+ 3 (* n 5)))
-(define (step-count-l n) (+ 3 (* n 5)))
-(define (step-count-m n) (+ 3 (* n 5)))
-(define (step-count-o n) (+ 3 (* n 5)))
+
+
+(define (compare-steps-k n) (+ 3 (* n 5)))
+(define (compare-steps-l n) (+ 3 (* n 5)))
+(define (compare-steps-m n) (+ 3 (* n 5)))
+(define (compare-steps-o n) (+ 3 (* n 5)))
 (define (step-count-t n) (+ 3 (* n 5)))
+
+(define (fib-steps n)
+  (if (> 2 n) 3 
+      (* 3
+         (fib-help (- n 2) 1 1))))
+(define (fib-help n fn-2 fn-1)
+  (if (= n 0) (+ fn-2 fn-1 2)
+      (fib-help (- n 1) fn-1 (+ fn-2 fn-1 2)))
+      ) 
+
+
+
+(check-expect (fib-steps 0) 3) ;; 3 1
+(check-expect (fib-steps 1) 3) ;; 0*3 + 3 1
+(check-expect (fib-steps 2) 12) ;; 3*3 + 3 4
+(check-expect (fib-steps 3) 21) ;; 6*3 + 3 7
+(check-expect (fib-steps 4) 39) ;; 12*3 + 3 13
+;; 66 21*3 + 3 22
+;; 111 36*3 + 3 37
