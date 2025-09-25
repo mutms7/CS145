9a060131fb969a8781664c4796720346f3d73fce
mutms7 <welcomehomemutms7@gmail.com>
2025-09-25 17:30:42 -0400

    w

--- ZLecture 6 Lists.rkt
+++ ZLecture 6 Lists.rkt
@@ -10,12 +10,12 @@
 (check-expect (nth L1 2) 10)
 (check-expect (nth L1 3) 15)
 (check-expect (nth L1 4) 20)
-(check-expect (nth L1 5) empty)
+(check-expect (nth L1 5) 'fuckyou)
 
 (define (nth L n) ;;useless    i lied useful
   (cond
     [(= n 1) (first L)]
-    [(empty? (rest L)) 'fuck you] ;; should throw exception exceeds index
+    [(empty? (rest L)) 'fuckyou] ;; should throw exception exceeds index
     [else (nth (rest L) (sub1 n))]
     )
   )
@@ -35,3 +35,56 @@ t2
 
 (define bigL2 (list 10 20 30 40 50 60 70 80 90))
 
+(empty? z3) ;; constant runtime
+(zero? (length z3)) ;; linear runtime NONO
+
+#|
+(define (my-list-ref L i) ;; all natural number functions
+  (cond
+    [(zero? i) ...]
+    [else ... i ... (my-list-ref ... (sub1 i))]
+    )
+  )
+|#
+
+(define (my-list-ref L i) ;; all natural number functions
+  (cond
+    [(zero? i) (first L)]
+    [else (my-list-ref (rest L) (sub1 i))]
+    )
+  )
+(define l (list 1 2 3 4 5))
+(check-expect (my-list-ref l 0) 1)
+(check-expect (my-list-ref l 1) 2)
+(check-expect (my-list-ref l 2) 3)
+(check-expect (my-list-ref l 3) 4)
+(check-expect (my-list-ref l 4) 5)
+
+(check-expect (my-length empty) 0)
+(check-expect (my-length l) 5)
+
+(define (my-length L)
+  (cond
+    [(empty? L) 0]
+    [else (add1 (my-length (rest L)))]
+)
+)
+
+(check-expect (my-member? 3 l) true)
+(check-expect (my-member? 33 l) false)
+
+(define (my-member? n L)
+  (cond
+    [(empty? L) false]
+    [(= (first L) n) true]
+    [else (my-member? n (rest L))]
+)
+)
+
+(define (my-append L1 L2)
+  (cond
+    [(empty? L1) false]
+    [(= (first L) n) true]
+    [else (my-member? n (rest L))]
+)
+)
