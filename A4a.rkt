626edb9653173c39750bda270557f2058c33a34f
mutms7 <welcomehomemutms7@gmail.com>
2025-09-27 14:13:16 -0400

    w

---
+++ A4a.rkt
@@ -0,0 +1,163 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname A4a) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define-struct node (left right sz))
+
+(define (tree-similar? t1 t2)
+  (cond
+    [(and (number? t1) (number? t2)) (= t1 t2)]
+    [(and (not (and (node? t1) (node? t2))) (or (node? t1) (node? t2))) false]
+    [(and (empty? t1) (empty? t2)) true]
+    [(or (empty? t1) (empty? t2)) false]
+    [else (and (tree-similar? (node-left t1) (node-left t2)) (tree-similar? (node-right t1) (node-right t2)))]
+            )
+  )
+
+(define (tree-create-min n)
+  (cond
+    [(= n 0) empty]
+    [(= n 1) (make-node empty empty 1)]
+    [else (make-node (tree-create-min (floor (/ (- n 1) 2)))
+                 (tree-create-min (ceiling (/ (- n 1) 2)))
+                 n)]
+  )
+  )
+
+
+
+(define (full-tree n)
+  (fthelp empty (ftheight n 0))
+  )
+
+(define (fthelp t h) ;; 
+  (cond
+    [(= h 0) t]
+    [(empty? t) (fthelp (make-node t t 1) (sub1 h))]
+    [else (fthelp (make-node t t (+ 1 (* 2 (node-sz t)))) (sub1 h))]
+  )
+)
+
+(define (ftheight n accum) ;; 1 = 1 ;; 8 4 2 1
+  (cond
+    [(= n 1) accum]
+    [else (ftheight (/ n 2) (+ 1 accum))]
+    )
+  )
+
+(define (tree-size t)
+  (if (empty? t) 0 (node-sz t)
+                      )
+      )
+ 
+
+;; 1
+
+(check-expect (tree-similar? (tree-grow-min (tree-create-min 6)) (tree-create-min 7)) true)
+(check-expect (tree-similar? (tree-grow-min (tree-create-min 2)) (tree-create-min 3)) true)
+
+
+
+(define (tree-grow-min n)
+  (cond
+    [(empty? n) (make-node empty empty 1)]
+    [(> (tree-size (node-left n)) (tree-size (node-right n)))
+       (make-node
+        (node-left n)
+        (tree-grow-min
+         (node-right n)) (add1 (node-sz n)))
+       ]
+    [else
+     (make-node
+        (tree-grow-min
+         (node-left n))
+        (node-right n)
+        (add1 (node-sz n)))
+       ]
+    )
+  )
+
+(check-expect (tree-similar? (tree-grow-min (tree-create-min 0)) (tree-create-min 1)) true)
+(check-expect (tree-similar? (tree-grow-min (tree-create-min 14)) (tree-create-min 15)) true)
+
+
+;; b
+
+(check-expect (tree-similar? (tree-shrink-min (tree-create-min 1)) (tree-create-min 0)) true)
+(check-expect (tree-similar? (tree-shrink-min (tree-create-min 4)) (tree-create-min 3)) true)
+
+
+
+(define (tree-height n)
+  (if (empty? n) 0
+      (+ 1 (max (tree-height (node-left n))
+                (tree-height (node-right n))
+                )
+         )
+      
+  )
+  )
+
+(define (istwo n)
+  (cond
+    [(= n 1) true]
+    [(= (remainder n 2) 1) false]
+    [else (istwo (/ n 2))]
+    )
+  )
+
+(define (tree-shrink-min n)
+  (cond
+    [(empty? n) empty]
+    [(and (empty? (node-right n)) (empty? (node-left n))) empty]
+    [(istwo (tree-size n)) (full-tree (tree-size n))]
+    [(empty? (node-right n))
+     (make-node
+        (tree-shrink-min
+         (node-left n))
+        (node-right n)
+        (sub1 (node-sz n)))
+     
+       ]
+    [(empty? (node-left n))
+     (make-node
+      (node-left n)
+      (tree-shrink-min
+         (node-right n))
+        (sub1 (node-sz n))
+        )
+       ]
+    [else
+     (make-node
+        (node-left n)
+        (tree-shrink-min
+         (node-right n)) (sub1 (node-sz n)))
+       ]
+    )
+  )
+
+(check-expect (tree-similar? (tree-shrink-min (tree-create-min 8)) (tree-create-min 7)) true)
+(check-expect (tree-similar? (tree-shrink-min (tree-create-min 2)) (tree-create-min 1)) true)
+(check-expect (tree-similar? (tree-shrink-min (tree-create-min 16)) (tree-create-min 15)) true)
+
+#|
+(define (tree-shrink-mink n)
+  (cond
+    [(empty? n) empty]
+    [(and (empty? (node-right n)) (empty? (node-left n))) empty]
+    [(> (tree-height (node-left n)) (tree-height (node-right n)))
+     (make-node
+        (tree-shrink-min
+         (node-left n))
+        (node-right n)
+        )
+     
+       ]
+    [else
+     (make-node
+        (node-left n)
+        (tree-shrink-min
+         (node-right n)))
+       ]
+    )
+  )
+|#
