c3100a5df64d700768d0e93dde91c8f23ffbe936
mutms7 <welcomehomemutms7@gmail.com>
2025-10-30 10:54:13 -0400

    s

--- Lecture 14 Infinitude.rkt
+++ Lecture 14 Infinitude.rkt
@@ -2,11 +2,12 @@
 
 (require "stream.rkt")
 
-(provide fibseq fibfirst fibrest)
 
 (define (my-if test true-part false-part)
   (if test (true-part) (false-part)))
 
 (define (why?) ((λ(x) (x x)) (λ(x) (x x))))
 
-
+(define s (list->stream (build-list 100000 add1)))
+(stream-first s)
+(stream-first (stream-rest s))
