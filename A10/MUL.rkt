23564875dd64705a2a66d05afc2d21879feb6a69
William Chenyin <welcomehomemutms7@gmail.com>
2025-11-26 22:57:40 -0500

    w

--- A10/MUL.rkt
+++ A10/MUL.rkt
@@ -2,7 +2,45 @@
 (require "Lambda.rkt")
 
 (define (MUL a b)
-  (MULh (removeZ a) (removeZ b))
+  (MULf (removeZ a) (removeZ b) (halve (removeZ a) Z))
+  )
+
+(define (MULf a b ah)
+  (MULp
+   (removeZ (front a ah))
+   (removeZ (back a ah))
+   (removeZ (front a ah))
+   (removeZ (back a ah))
+   ah))
+
+(define (MULp a b c d m)
+  (MULd (MULt (ADD a b) (ADD c d)) (MULt a c) (MULt b d) m))
+
+(define (MULd big fro bac m)
+  (ADD (shift (shift bac m) m)
+       (ADD (shift (removeZ (SUB (removeZ (SUB big bac)) fro)) m)
+            fro)))
+
+
+(define (SUB1 x)
+   (If (Z? x) Z
+       (If (First x) (Cons False (Rest x))
+           (Cons True (SUB1 (Rest x))))))
+
+(define (SUB a b)
+  (If (Z? a) Z
+   (If (Z? b) a
+       ;; first b is false; first digit is first a
+    (If (Not (First b)) (Cons (First a) (SUB (Rest a) (Rest b)))
+        ;; first a is true; first digit is not first b
+     (If (First a) (Cons (Not (First b)) (SUB (Rest a) (Rest b)))
+         ;; first a is false, first b is true, then true and sub1 to rest
+       (Cons True (SUB1 (removeZ (SUB (Rest a) (Rest b))))))))))
+   
+
+(define (MULt a b)
+  (If (Z? (removeZ b)) Z
+  (MULh (removeZ a) (removeZ b)))
   )
 
 (define (MULh a b) ;;101 0011
@@ -51,7 +89,7 @@
            ))
 
 (define (shift a amt)
-  (If (Z? amt) a
+  (If (Z? amt)  (Cons False a)
       (shift (Cons False a) (Rest amt))
            ))
 
